af5b5bade74c9bef7add120f17054dad1f4a4e0b
elmaaroufi1 <abdessamad.maaroufi12@gmail.com>
2023-02-12 04:44:29 +0100

    update-project

--- proanalyse/urls.py
+++ proanalyse/urls.py
@@ -11,3 +11,5 @@ urlpatterns = [
     path('data_analysis/',include('data_analysis.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
